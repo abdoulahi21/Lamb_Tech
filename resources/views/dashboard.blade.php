833b764b7920902fddba48df1fa22f882f82150a
diariatoudiame <diamediariatou344@gmail.com>
2024-05-05 08:55:16 +0000

    Modification Routess

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -111,7 +111,7 @@
                                     </button>
                                     <ul id="dropdown-crud" class="space-y-2 py-2 hidden ">
                                         <li>
-                                            <a href="{{ route('schoolclass.index') }}" class="bg-white text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100 transition duration-75 pl-11 dark:text-gray-200 dark:hover:bg-gray-700 bg-gray-100 dark:bg-gray-700">
+                                            <a href="{{ route('manager.schoolclass.index') }}" class="bg-white text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100 transition duration-75 pl-11 dark:text-gray-200 dark:hover:bg-gray-700 bg-gray-100 dark:bg-gray-700">
                                                 Classes
                                             </a>
 
@@ -490,7 +490,7 @@
                 </button>
                 <ul id="dropdown-crud2" class="space-y-2 py-2 hidden ">
                     <li>
-                        <a href="{{ route('schoolclass.index') }}" class="bg-white text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100 transition duration-75 pl-11 dark:text-gray-200 dark:hover:bg-gray-700 bg-gray-100 dark:bg-gray-700">
+                        <a href="{{ route('manager.schoolclass.index') }}" class="bg-white text-base text-gray-900 rounded-lg flex items-center p-2 group hover:bg-gray-100 transition duration-75 pl-11 dark:text-gray-200 dark:hover:bg-gray-700 bg-gray-100 dark:bg-gray-700">
                             Classes
                         </a>
                     </li>
